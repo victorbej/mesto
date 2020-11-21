38895b6bfc33176edc17fa090d66bd92b2cf6df6
Victor Bej <victorbejenar@yandex.ru>
2020-11-21 21:15:34 +0300

    popup added

---
+++ index.js
@@ -0,0 +1,14 @@
+// const headerButtonNode = document.querySelector('.profile__reduction-button');
+// const popupNode = document.querySelector('.popup');
+// const popupCloseButtonNode = document.querySelector('.popup__close-button');
+
+// const leadTitleNode = document.querySelector('.lead__title');
+// const formButtonNode = document.querySelector('.form__button');
+
+// headerButtonNode.addEventListener('click', togglePopupVisibility);
+// popupCloseButtonNode.addEventListener('click', togglePopupVisibility);
+
+// function togglePopupVisibility() {
+//     popupNode.classList.toggle('popup_visible');
+// }
+// }
