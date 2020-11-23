277b6654745a2ccb5ebcff8f2686dac5c2db86fa
Victor Bej <victorbejenar@yandex.ru>
2020-11-23 22:28:13 +0300

    js full form and complete project before review

--- index.js
+++ index.js
@@ -1,57 +1,40 @@
-let popup = document.querySelector('.popup');
-
-let reductionButton = document.querySelector('.profile__reduction-button');
-reductionButton.addEventListener('click', activateReductionButton);
+const popup = document.querySelector('.popup');
+const popupReductionButton = document.querySelector('.profile__reduction-button');
+const popupCloseButton = document.querySelector('.popup__close-button');
+const profileTitle = document.querySelector('.profile__title');
+const profileText = document.querySelector('.profile__text');
+const profileTitleInput = document.querySelector('.popup__formfield-input_name');
+const profileTextInput = document.querySelector('.popup__formfield-input_job');
+const formElement = document.querySelector('.popup__container');
+const popupEdit = document.querySelector('.popup__edit');
+const popupSaveButton = document.querySelector('.popup__save-button');
 
 function activateReductionButton() {
     popup.classList.add('popup_opened');
 };
 
-let popupCloseButton = document.querySelector('.popup__close-button');
-popupCloseButton.addEventListener('click', deactivateReductionButton);
-
 function deactivateReductionButton() {
     popup.classList.remove('popup_opened');
 };
 
-let profileTitle = document.querySelector('.profile__title');
-// profileTitle.innerHTML = 'Жак-Ив Кусто'
+function deactivateSaveButton() {
+    popup.classList.remove('popup_opened');
+};
 
-let profileText = document.querySelector('.profile__text');
-profileText.innerHTML = 'Исследователь океана';
-
-// Находим форму в DOM
-let formElement = document.querySelector('.popup__container') // Воспользуйтесь методом querySelector()
-
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                        // Так мы можем определить свою логику отправки.
-                        // О том, как это делать, расскажем позже.
-
-    // Находим поля формы в DOM
-    let name = document.querySelector('.popup__formfield-input_name'); // Воспользуйтесь инструментом .querySelector()
-    let job = document.querySelector('.popup__formfield-input_job');// Воспользуйтесь инструментом .querySelector()
-
-    console.log(name.value);
-    console.log(job.value);// Получите значение полей из свойства value
-
-    profileText.insertAdjacentHTML('beforeend', 
-    
-    `<h1 class="profile__title">${name.value}</h1>`);
-
-    name.value = 'Жак-Ив Кусто';
-    
-    // Выберите элементы, куда должны быть вставлены значения полей
-
-    profileTitle.textContent = name.value
-    profileText.textContent = job.value// Вставьте новые значения с помощью textContent
+function insertProfileInfo () {
+    profileTitleInput.value = profileTitle.textContent;
+    profileTextInput.value = profileText.textContent;
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', formSubmitHandler); 
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    profileTitle.textContent = profileTitleInput.value;
+    profileText.textContent = profileTextInput.value;
+}
 
-let saveButton = document.querySelector('.profile__save-button');
-saveButton.addEventListener('click', formElement);
+
+insertProfileInfo();
+popupCloseButton.addEventListener('click', deactivateReductionButton);
+popupReductionButton.addEventListener('click', activateReductionButton); 
+popupSaveButton.addEventListener('click', deactivateSaveButton);
+formElement.addEventListener('submit', formSubmitHandler);
